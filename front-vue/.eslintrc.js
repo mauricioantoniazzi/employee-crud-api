d98633635f7b41cafcc77b923a635ced77510323
mauricio29 <mauricio@decisaosistemas.com.br>
2021-03-05 14:28:30 -0300

    Desenvolvimento da inclusão do NProgress na aplicação.

--- front-vue/.eslintrc.js
+++ front-vue/.eslintrc.js
@@ -11,6 +11,9 @@ module.exports = {
     parser: 'babel-eslint',
   },
   rules: {
+    'prefer-destructuring': 'off',
+    'no-unused-vars': 'off',
+    'no-useless-return': 'off',
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
